c6318c9264f477304dabccd2219a7f83eb751ce0
Enrique Hernandez <enirque_inf@homail.com>
2022-06-23 13:09:05 -0500

    agregando herencia a JS

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,7 @@
 var car = new Car("DEV2115", new Account("Luis Enrique", "mydocument"))
 car.passenger = 4
 car.printDataCar()
-console.log(car.passenger);
+console.log(car.passenger)
+
+var uberX = new UberX("DEV2116", new Account("Jose Miguel", "MyCard"), "Ford", "Focus RS")
+uberX.printDataCar()
